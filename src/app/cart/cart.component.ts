e76cdebc110555548f549ba724a26ddaac47af40
Nicholas Gilbertson <gilb0578@morris.umn.edu>
2022-02-13 16:21:50 -0600

    Implemented ability to checkout and input name/address

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartService } from '../cart.service';
 
 @Component({
@@ -10,6 +11,21 @@ export class CartComponent {
 
   items = this.cartService.getItems();
 
-  constructor(private cartService: CartService) { }
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
+    ) { }
+
+    onSubmit(): void {
+      // Process checkout data here
+      this.items = this.cartService.clearCart();
+      console.warn('Your order has been submitted', this.checkoutForm.value);
+      this.checkoutForm.reset();
+    }
 
 }
