3d5d100e6fef9a497e6d333dcbc7454c58cb0fb0
Nicholas Gilbertson <gilb0578@morris.umn.edu>
2022-02-13 16:05:31 -0600

    Implemented shipping prices page

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 
 @Injectable({
@@ -6,7 +7,7 @@ import { Product } from './products';
 })
 export class CartService {
   items: Product[] = [];
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -19,5 +20,8 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
 }
