296b969d8673b1c9fabb207aa432fadcdfaba6a6
Nicholas Gilbertson <gilb0578@morris.umn.edu>
2022-02-13 15:55:28 -0600

    Implemented ability to add items to cart page

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class CartComponent {
 
-  constructor() { }
+  items = this.cartService.getItems();
+
+  constructor(private cartService: CartService) { }
 
 }
