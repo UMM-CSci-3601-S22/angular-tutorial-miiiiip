9454f75aa59ae2e74a42eb23ab7ca2b66ee22b00
Nicholas Gilbertson <gilb0578@morris.umn.edu>
2022-02-13 14:39:10 -0600

    Removing onInit component to appease ng lint

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
@@ -6,15 +6,12 @@ import { Product } from '../products';
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css']
 })
-export class ProductAlertsComponent implements OnInit {
+export class ProductAlertsComponent {
 
 
   @Input() product: Product | undefined;
   @Output() notify = new EventEmitter();
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
 
